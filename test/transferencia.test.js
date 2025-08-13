9b13ff950d47372466ca026a4f37b32a7ec033d7
Natan <natan_m.mendonca@outlook.com>
2025-08-12 23:32:16 -0300

    adicionando fixtures para gerir os corpos das requisições

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,7 +1,8 @@
 const request = require('supertest');
 const {expect} = require('chai')
 require('dotenv').config()
-const {obterToken} = require('../helpers/autenticacao');
+const {obterToken} = require('../helpers/autenticacao')
+const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe('transferencias', ()=>{
     describe('POST / transferencias', ( )=> {
@@ -12,35 +13,30 @@ describe('transferencias', ()=>{
         }) 
 
         it('Deve retornar sucesso com 201 quando o valor da transferência for acima de R$10.00', async () => {             
+            const bodyTransferencias  = { ...postTransferencias }
+            
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    'conta_origem': 1,
-                    'conta_destino': 2, 
-                    'valor': 7,
-                    'token': ""
-                })
+                .send(bodyTransferencias)
+
                  expect(resposta.status).to.equal(422);        
                  
-                 console.log(resposta.body)
          })
 
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$10.00', async () => {  
+            const bodyTransferencias  = { ...postTransferencias }
+            bodyTransferencias.valor = 7
+
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    'conta_origem': 1,
-                    'conta_destino': 2, 
-                    'valor': 11,
-                    'token': ""
-                })
+                .send(bodyTransferencias)
+
                  expect(resposta.status).to.equal(201);        
                  
-                 console.log(resposta.body)
          })    
          
         })
